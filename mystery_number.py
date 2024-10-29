aaa631bcef618ca29dad1dfecde77ab5ef9c7a86
thomas <bezille.thomasb@gmail.com>
2024-10-29 16:48:44 +0100

    📦 NEW: If the number is greater or smaller

--- mystery_number.py
+++ mystery_number.py
@@ -16,3 +16,11 @@ while True:
     elif int(try_nbr) < MIN or int(try_nbr) > MAX:
         print(f"Le nombre doit être entre {MIN} et {MAX}.")
         continue
+    elif int(try_nbr) < NBR_MYSTERY:
+        print(f"Le nombre mystère est plus grand que {try_nbr}.")
+        life -= 1
+        print(f"Il vous reste {life} essais.")
+    elif int(try_nbr) > NBR_MYSTERY:
+        print(f"Le nombre mystère est plus petit que {try_nbr}.")
+        life -= 1
+        print(f"Il vous reste {life} essais.")
